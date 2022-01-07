f7d69d1670fc2888cc648d24827d2c7d8f6464b9
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2022-01-07 11:42:48 +0600

    props passing in style component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import StyleButton from "./components/Button";
 function App() {
   return (
     <div className="App">
-      <button>Button</button>
       <StyleButton>Button</StyleButton>
+      <div>
+        <br/>
+      </div>
+      <StyleButton variant='outline'>Button</StyleButton>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,16 @@ import styled from 'styled-components';
 
 
 const StyleButton=styled.button`
-  color:red;
+  border:2px solid #4caf50;
+  background-color: ${(props)=>props.variant==='outline'?'#FFF':'#4caf50'};
+  color:  ${(props)=>props.variant==='outline'?'#4caf50':'#FFF'};
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  transition:0.5s all ease-out;
 `
 
 export default StyleButton;
